2baad4d4b7c97c4fbc5299f9d153c1714c52cd0e
gqbo <gabriel.gonzaf@gmail.com>
2025-04-06 16:55:47 -0600

    feat(register): add RegisterState class and related states for registration flow

---
+++ lib/src/auth/_children/register/presenter/bloc/register_state.dart
@@ -0,0 +1,48 @@
+part of 'register_bloc.dart';
+
+sealed class RegisterState extends Equatable {
+  const RegisterState();
+  
+  @override
+  List<Object> get props => [];
+}
+
+final class RegisterInitial extends RegisterState {}
+
+final class RegisterLoading extends RegisterState {}
+
+final class RegisterEmailVerificationSent extends RegisterState {
+  final String email;
+
+  const RegisterEmailVerificationSent({required this.email});
+
+  @override
+  List<Object> get props => [email];
+}
+
+class RegisterEmailVerificationChecked extends RegisterEvent {
+  final User user;
+
+  const RegisterEmailVerificationChecked({required this.user});
+
+  @override
+  List<Object> get props => [user];
+}
+
+final class RegisterSuccess extends RegisterState {
+  final User user;
+
+  const RegisterSuccess({required this.user});
+
+  @override
+  List<Object> get props => [user];
+}
+
+final class RegisterFailure extends RegisterState {
+  final String error;
+
+  const RegisterFailure({required this.error});
+
+  @override
+  List<Object> get props => [error];
+}
