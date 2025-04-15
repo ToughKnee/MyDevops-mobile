5f1d2c125c4c8132d78384e3e5391dcbdbf60c98
FerAC <87688596+FerAC@users.noreply.github.com>
2025-04-14 19:51:52 -0600

    refactor(login): add isEnabled property to LoginButton for better control over button state

--- lib/src/auth/_children/login/presenter/widgets/button.dart
+++ lib/src/auth/_children/login/presenter/widgets/button.dart
@@ -4,20 +4,22 @@ class LoginButton extends StatelessWidget {
   final VoidCallback onPressed;
   final bool isLoading;
   final Text text;
-
+  final bool isEnabled;
   const LoginButton({
     super.key,
     required this.onPressed,
     required this.isLoading,
     required this.text,
+    required this.isEnabled,
   });
 
   @override
   Widget build(BuildContext context) {
     return ElevatedButton(
-      onPressed: () {
+      
+      onPressed: isEnabled? () {
         onPressed();
-      },
+      }: null,
       style: ElevatedButton.styleFrom(
         backgroundColor: Theme.of(context).colorScheme.primary,
         padding: EdgeInsets.all(15),
