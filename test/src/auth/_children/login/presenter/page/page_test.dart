cdc671b387b3e5656b64dd51e90f1257a0db577b
FerAC <87688596+FerAC@users.noreply.github.com>
2025-04-16 01:12:08 -0600

    test(login): enhance LoginPage tests to cover loading state and ensure proper UI behavior

--- test/src/auth/_children/login/presenter/page/page_test.dart
+++ test/src/auth/_children/login/presenter/page/page_test.dart
@@ -2,15 +2,18 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:bloc_test/bloc_test.dart';
+import 'package:mobile/core/storage/user_session.storage.dart';
 import 'package:mobile/src/auth/auth.dart';
+import 'package:mockito/mockito.dart';
 import 'package:shared_preferences/shared_preferences.dart';
-
+import '../bloc/login_bloc_test.mocks.dart';
 class MockLoginBloc extends MockBloc<LoginEvent, LoginState>
     implements LoginBloc {}
 
 void main() {
+  SharedPreferences.setMockInitialValues({});
   late MockLoginBloc mockLoginBloc;
-
+  late MockLocalStorage mockLocalStorage;
   final testUser = AuthUserInfo(
     id: 'test-user-id',
     email: 'user@test.com',
@@ -18,8 +21,7 @@ void main() {
   );
 
   setUp(() async {
-    SharedPreferences.setMockInitialValues({});
-
+    mockLocalStorage = MockLocalStorage();
     mockLoginBloc = MockLoginBloc();
   });
 
@@ -66,8 +68,28 @@ void main() {
         ),
       );
 
-      expect(find.byType(Container), findsOneWidget);
+      expect(find.byType(Center), findsOneWidget);
     });
+    testWidgets('doesnt show login Form when LoginBloc is loading',
+    (WidgetTester tester) async {
+      whenListen(
+        mockLoginBloc,
+        Stream.fromIterable([LoginLoading()]),
+        initialState: LoginLoading(),
+      );
+
+      await tester.pumpWidget(
+        MaterialApp(
+          home: BlocProvider<LoginBloc>.value(
+            value: mockLoginBloc,
+            child: const LoginPage(),
+          ),
+        ),
+      );
+
+      expect(find.byType(LoginForm), findsNothing);
+    }
+    );
 
     testWidgets('navigates to HomePage on LoginSuccess',
         (WidgetTester tester) async {
@@ -109,6 +131,23 @@ void main() {
 
       expect(find.byType(ScaffoldMessenger), findsOneWidget);
     });
-    
+
+    testWidgets('Displays HomePage Correctly', 
+    (WidgetTester tester) async {
+      LocalStorage.init();
+      when(mockLocalStorage.userId).thenReturn(testUser.id);
+      when(mockLocalStorage.userEmail).thenReturn(testUser.email);
+      
+      await tester.pumpWidget(
+        MaterialApp(
+          home: BlocProvider<LoginBloc>.value(
+            value: mockLoginBloc,
+            child: const HomePage(),
+          ),
+        ),
+      );
+
+      expect(find.text('Home'), findsOneWidget);
+    });
   });
 }
